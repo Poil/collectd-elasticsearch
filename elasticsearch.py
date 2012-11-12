02a57424243c4ae21c3625eeb3e85ae6689b760a
Poil <poil@quake.fr>
2012-11-12 13:24:41 +0100

    Rename TypeInstance for CGraphz compatiblity T(-STI)-TI

--- elasticsearch.py
+++ elasticsearch.py
@@ -16,86 +16,86 @@ STAT=dict()
 
 # INDICES METRICS #
 ## CACHE
-STAT['indices.cache.field.eviction'] = { "type": "counter", "path": "nodes.%s.indices.cache.field_evictions" }
-STAT['indices.cache.field.size'] = { "type": "bytes", "path": "nodes.%s.indices.cache.field_size_in_bytes" }
-STAT['indices.cache.filter.count'] = { "type": "counter", "path": "nodes.%s.indices.cache.filter_count" }
-STAT['indices.cache.filter.evictions'] = { "type": "counter", "path": "nodes.%s.indices.cache.filter_evictions" }
-STAT['indices.cache.filter.size'] = { "type": "bytes", "path": "nodes.%s.indices.cache.filter_size_in_bytes" }
+STAT['indices_cache_evictions-filter'] = { "type": "counter", "path": "nodes.%s.indices.cache.filter_evictions" }
+STAT['indices_cache_evictions-field'] = { "type": "counter", "path": "nodes.%s.indices.cache.field_evictions" }
+STAT['indices_cache_size-filter'] = { "type": "bytes", "path": "nodes.%s.indices.cache.filter_size_in_bytes" }
+STAT['indices_cache_size-field'] = { "type": "bytes", "path": "nodes.%s.indices.cache.field_size_in_bytes" }
+STAT['indices_cache_count-filter'] = { "type": "counter", "path": "nodes.%s.indices.cache.filter_count" }
 
 ## DOCS
-STAT['indices.docs.count'] = { "type": "gauge", "path": "nodes.%s.indices.docs.count" }
-STAT['indices.docs.deleted'] = { "type": "counter", "path": "nodes.%s.indices.docs.deleted" }
+STAT['indices_docs-count'] = { "type": "gauge", "path": "nodes.%s.indices.docs.count" }
+STAT['indices_docs-deleted'] = { "type": "counter", "path": "nodes.%s.indices.docs.deleted" }
 
 ## FLUSH
-STAT['indices.flush.total'] = { "type": "counter", "path": "nodes.%s.indices.flush.total" }
-STAT['indices.flush.time'] = { "type": "counter", "path": "nodes.%s.indices.flush.total_time_in_millis" }
+STAT['indices_total-flush'] = { "type": "counter", "path": "nodes.%s.indices.flush.total" }
+STAT['indices_time-flush'] = { "type": "counter", "path": "nodes.%s.indices.flush.total_time_in_millis" }
 
 ## GET
-STAT['indices.get.exists-time'] = { "type": "counter", "path": "nodes.%s.indices.get.exists_time_in_millis" }
-STAT['indices.get.exists-total'] = { "type": "counter", "path": "nodes.%s.indices.get.exists_total" }
-STAT['indices.get.time'] = { "type": "counter", "path": "nodes.%s.indices.get.time_in_millis" }
-STAT['indices.get.total'] = { "type": "counter", "path": "nodes.%s.indices.get.total" }
-STAT['indices.get.missing-time'] = { "type": "counter", "path": "nodes.%s.indices.get.missing_time_in_millis" }
-STAT['indices.get.missing-total'] = { "type": "counter", "path": "nodes.%s.indices.get.missing_total" }
+STAT['indices_get_time-current'] = { "type": "counter", "path": "nodes.%s.indices.get.time_in_millis" }
+STAT['indices_get_time-exists'] = { "type": "counter", "path": "nodes.%s.indices.get.exists_time_in_millis" }
+STAT['indices_get_time-missing'] = { "type": "counter", "path": "nodes.%s.indices.get.missing_time_in_millis" }
+STAT['indices_get_total-current'] = { "type": "counter", "path": "nodes.%s.indices.get.total" }
+STAT['indices_get_total-exists'] = { "type": "counter", "path": "nodes.%s.indices.get.exists_total" }
+STAT['indices_get_total-missing'] = { "type": "counter", "path": "nodes.%s.indices.get.missing_total" }
 
 ## INDEXING
-STAT['indices.indexing.delete-time'] = { "type": "counter", "path": "nodes.%s.indices.indexing.delete_time_in_millis" }
-STAT['indices.indexing.delete-total'] = { "type": "counter", "path": "nodes.%s.indices.indexing.delete_total" }
-STAT['indices.indexing.index-time'] = { "type": "counter", "path": "nodes.%s.indices.indexing.index_time_in_millis" }
-STAT['indices.indexing.index-total'] = { "type": "counter", "path": "nodes.%s.indices.indexing.index_total" }
+STAT['indices_indexing_time-delete'] = { "type": "counter", "path": "nodes.%s.indices.indexing.delete_time_in_millis" }
+STAT['indices_indexing_time-index'] = { "type": "counter", "path": "nodes.%s.indices.indexing.index_time_in_millis" }
+STAT['indices_indexing_total-delete'] = { "type": "counter", "path": "nodes.%s.indices.indexing.delete_total" }
+STAT['indices_indexing_total-index'] = { "type": "counter", "path": "nodes.%s.indices.indexing.index_total" }
 
 ## MERGES
-STAT['indices.merges.current'] = { "type": "gauge", "path": "nodes.%s.indices.merges.current" }
-STAT['indices.merges.current-docs'] = { "type": "gauge", "path": "nodes.%s.indices.merges.current_docs" }
-STAT['indices.merges.current-size'] = { "type": "bytes", "path": "nodes.%s.indices.merges.current_size_in_bytes" }
-STAT['indices.merges.total'] = { "type": "counter", "path": "nodes.%s.indices.merges.total" }
-STAT['indices.merges.total-docs'] = { "type": "gauge", "path": "nodes.%s.indices.merges.total_docs" }
-STAT['indices.merges.total-size'] = { "type": "bytes", "path": "nodes.%s.indices.merges.total_size_in_bytes" }
-STAT['indices.merges.time'] = { "type": "counter", "path": "nodes.%s.indices.merges.total_time_in_millis" }
+STAT['indices_merges-current'] = { "type": "gauge", "path": "nodes.%s.indices.merges.current" }
+STAT['indices_merges-total'] = { "type": "counter", "path": "nodes.%s.indices.merges.total" }
+STAT['indices_merges_docs-current'] = { "type": "gauge", "path": "nodes.%s.indices.merges.current_docs" }
+STAT['indices_merges_docs-total'] = { "type": "gauge", "path": "nodes.%s.indices.merges.total_docs" }
+STAT['indices_merges_size-current'] = { "type": "bytes", "path": "nodes.%s.indices.merges.current_size_in_bytes" }
+STAT['indices_merges_size-total'] = { "type": "bytes", "path": "nodes.%s.indices.merges.total_size_in_bytes" }
+STAT['indices_merges_time-total'] = { "type": "counter", "path": "nodes.%s.indices.merges.total_time_in_millis" }
 
 ## REFRESH
-STAT['indices.refresh.total'] = { "type": "counter", "path": "nodes.%s.indices.refresh.total" }
-STAT['indices.refresh.time'] = { "type": "counter", "path": "nodes.%s.indices.refresh.total_time_in_millis" }
+STAT['indices_total-refresh'] = { "type": "counter", "path": "nodes.%s.indices.refresh.total" }
+STAT['indices_time-refresh'] = { "type": "counter", "path": "nodes.%s.indices.refresh.total_time_in_millis" }
 
 ## SEARCH
-STAT['indices.search.query-current'] = { "type": "gauge", "path": "nodes.%s.indices.search.query_current" }
-STAT['indices.search.query-total'] = { "type": "counter", "path": "nodes.%s.indices.search.query_total" }
-STAT['indices.search.query-time'] = { "type": "counter", "path": "nodes.%s.indices.search.query_time_in_millis" }
-STAT['indices.search.fetch-current'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_current" }
-STAT['indices.search.fetch-total'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_total" }
-STAT['indices.search.fetch-time'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_time_in_millis" }
+STAT['indices_search_total-query'] = { "type": "counter", "path": "nodes.%s.indices.search.query_total" }
+STAT['indices_search_current-query'] = { "type": "gauge", "path": "nodes.%s.indices.search.query_current" }
+STAT['indices_search_fetch-current'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_current" }
+STAT['indices_search_fetch-total'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_total" }
+STAT['indices_search_time-fetch'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_time_in_millis" }
+STAT['indices_search_time-query'] = { "type": "counter", "path": "nodes.%s.indices.search.query_time_in_millis" }
 
 ## STORE
-STAT['indices.store.size'] = { "type": "bytes", "path": "nodes.%s.indices.store.size_in_bytes" }
+STAT['indices_size-store'] = { "type": "bytes", "path": "nodes.%s.indices.store.size_in_bytes" }
 
 # JVM METRICS #
 ## MEM
-STAT['jvm.mem.heap-committed'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.heap_committed_in_bytes" }
-STAT['jvm.mem.heap-used'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.heap_used_in_bytes" }
-STAT['jvm.mem.non-heap-committed'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.non_heap_committed_in_bytes" }
-STAT['jvm.mem.non-heap-used'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.non_heap_used_in_bytes" }
+STAT['jvm_mem-heap_committed'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.heap_committed_in_bytes" }
+STAT['jvm_mem-heap_used'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.heap_used_in_bytes" }
+STAT['jvm_mem-non_heap_committed'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.non_heap_committed_in_bytes" }
+STAT['jvm_mem-non_heap_used'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.non_heap_used_in_bytes" }
 
 ## THREADS
-STAT['jvm.threads.count'] = { "type": "gauge", "path": "nodes.%s.jvm.threads.count" }
-STAT['jvm.threads.peak'] = { "type": "gauge", "path": "nodes.%s.jvm.threads.peak_count" }
+STAT['jvm_threads-count'] = { "type": "gauge", "path": "nodes.%s.jvm.threads.count" }
+STAT['jvm_threads-peak'] = { "type": "gauge", "path": "nodes.%s.jvm.threads.peak_count" }
 
 ## GC
-STAT['jvm.gc.time'] = { "type": "counter", "path": "nodes.%s.jvm.gc.collection_time_in_millis" }
-STAT['jvm.gc.count'] = { "type": "counter", "path": "nodes.%s.jvm.gc.collection_count" }
+STAT['jvm_gc-time'] = { "type": "counter", "path": "nodes.%s.jvm.gc.collection_time_in_millis" }
+STAT['jvm_gc-count'] = { "type": "counter", "path": "nodes.%s.jvm.gc.collection_count" }
 
 # TRANSPORT METRICS #
-STAT['transport.server_open'] = { "type": "gauge", "path": "nodes.%s.transport.server_open" }
-STAT['transport.rx.count'] = { "type": "counter", "path": "nodes.%s.transport.rx_count" }
-STAT['transport.rx.size'] = { "type": "bytes", "path": "nodes.%s.transport.rx_size_in_bytes" }
-STAT['transport.tx.count'] = { "type": "counter", "path": "nodes.%s.transport.tx_count" }
-STAT['transport.tx.size'] = { "type": "bytes", "path": "nodes.%s.transport.tx_size_in_bytes" }
+STAT['transport_open-server'] = { "type": "gauge", "path": "nodes.%s.transport.server_open" }
+STAT['transport_count-rx'] = { "type": "counter", "path": "nodes.%s.transport.rx_count" }
+STAT['transport_count-tx'] = { "type": "counter", "path": "nodes.%s.transport.tx_count" }
+STAT['transport_size-rx'] = { "type": "bytes", "path": "nodes.%s.transport.rx_size_in_bytes" }
+STAT['transport_size-tx'] = { "type": "bytes", "path": "nodes.%s.transport.tx_size_in_bytes" }
 
 # HTTP METRICS #
-STAT['http.current_open'] = { "type": "gauge", "path": "nodes.%s.http.current_open" }
-STAT['http.total_open'] = { "type": "gauge", "path": "nodes.%s.http.total_opened" }
+STAT['http_open-current'] = { "type": "gauge", "path": "nodes.%s.http.current_open" }
+STAT['http_open-total'] = { "type": "gauge", "path": "nodes.%s.http.total_opened" }
 
 # PROCESS METRICS #
-STAT['process.open_file_descriptors'] = { "type": "gauge", "path": "nodes.%s.process.open_file_descriptors" }
+STAT['process-open_file_descriptors'] = { "type": "gauge", "path": "nodes.%s.process.open_file_descriptors" }
 
 # FUNCTION: Collect stats from JSON result
 def lookup_stat(stat, json):
